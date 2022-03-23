983505abfdb79e72713e211adaa9cf9a63acd1d3
Alan Kemboi <alankemboi@gmail.com>
2022-03-23 18:22:32 +0300

    Layout

--- bot.js
+++ bot.js
@@ -42,6 +42,10 @@ bot.command('start', ctx => {
         [Markup.urlButton('Add me to chat!', 'https://t.me')]
     )).HTML())
 })
+bot.command('link', ctx => {
+    const text = '[Link here](href="https://www.paypal.com)';
+    ctx.reply(text, parse_mode = 'MarkdownV2')
+})
 bot.command('donate', ctx => {
     ctx.reply('So you want to donate? Amazing! It took alot of work for my creator to get me where I am now - so if you have some money to spare, and want to show your support; Donate! After all, server fees dont pay themselves - so every little helps! All donation money goes straight to funding the VPS, and of course boosting morale - always nice to see my work appreciated:)\n\nYou can donate on M-PESA here,\nor PAYPAL ,<a>here</a>.\n\nIf you want tp help support me monthly basis,you can set up recurring donation on Github Sponsors.\n\nThank you for your generosity!')
 })
