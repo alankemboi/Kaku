04014ec854ce371d6422a4455d6084832a4b4198
Alan Kemboi <alankemboi@gmail.com>
2022-03-23 14:19:39 +0300

    Fix error

--- bot.js
+++ bot.js
@@ -46,7 +46,6 @@ bot.command('start', ctx => {
         }
     })
 })
-bot.start((ctx) => ctx.reply(`Deep link payload: ${ctx.startPayload}`))
 bot.command('donate', ctx => {
     bot.telegram.sendMessage(ctx.chat.id, 'So you want to donate? Amazing! It took alot of work for my creator to get me where I am now - so if you have some money to spare, and want to show your support; Donate! After all, server fees dont pay themselves - so every little helps! All donation money goes straight to funding the VPS, and of course boosting morale - always nice to see my work appreciated:)\n\nYou can donate on M-PESA here,\nor PAYPAL here.\n\nIf you want tp help support me monthly basis,you can set up recurring donation on Github Sponsors.\n\nThank you for your generosity!')
 })
